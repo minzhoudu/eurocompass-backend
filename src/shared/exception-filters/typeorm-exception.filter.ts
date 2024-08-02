f14cc44762fb9a88ad00443c0e1dede2bd8ab240
Pavle Jovanovic <pavle.jovanovic@source.ag>
2024-08-02 14:33:47 +0200

    exception filter status code enum

--- src/shared/exception-filters/typeorm-exception.filter.ts
+++ src/shared/exception-filters/typeorm-exception.filter.ts
@@ -1,6 +1,11 @@
 import { EntityNotFoundError, QueryFailedError } from 'typeorm';
 
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
 
 @Catch(QueryFailedError, EntityNotFoundError)
 export class TypeormExceptionFilter implements ExceptionFilter {
@@ -9,22 +14,22 @@ export class TypeormExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse();
 
     if (exception.code === '23505') {
-      return response.status(409).json({
-        statusCode: 409,
+      return response.status(HttpStatus.CONFLICT).json({
+        statusCode: HttpStatus.CONFLICT,
         message: 'Email already exists',
       });
     }
 
     if (exception instanceof EntityNotFoundError) {
-      return response.status(404).json({
-        statusCode: 404,
+      return response.status(HttpStatus.NOT_FOUND).json({
+        statusCode: HttpStatus.NOT_FOUND,
         message: 'Entity not found',
       });
     }
 
     if (exception instanceof QueryFailedError) {
-      return response.status(409).json({
-        statusCode: 409,
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        statusCode: HttpStatus.BAD_REQUEST,
         message: exception.message,
       });
     }
