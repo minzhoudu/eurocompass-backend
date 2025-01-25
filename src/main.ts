6f16ca08607aa99bebeccc8d38fa57bf375cd5c4
Pavle <jovanovix.p@gmail.com>
2025-01-25 14:09:59 +0100

    adding engines

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://eurocompass.site/'],
     credentials: true,
   });
   app.use(cookieParser());
