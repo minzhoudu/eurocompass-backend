3258dc86b1cef7f010919adb2cbc432adc1f5980
Pavle <jovanovix.p@gmail.com>
2025-01-25 14:50:15 +0100

    cors update for deployment preview

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://eurocompass.site/'],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:5173',
+        'https://eurocompass.site/',
+      ];
+      const netlifyPattern = /\.eurocompass\.netlify\.app$/;
+
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        netlifyPattern.test(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
   app.use(cookieParser());
